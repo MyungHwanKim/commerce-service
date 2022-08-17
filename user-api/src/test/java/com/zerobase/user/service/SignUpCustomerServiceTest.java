2e5490c5c2be1211834091981146812c2af79c98
MyungHwanKim <code.kmh9250@gmail.com>
2022-08-18 03:31:42 +0900

    test : [002-SignUp] customer에 대한 SignUp Service test 구현

---
+++ user-api/src/test/java/com/zerobase/user/service/SignUpCustomerServiceTest.java
@@ -0,0 +1,38 @@
+package com.zerobase.user.service;
+
+import com.zerobase.user.domain.SignUpForm;
+import com.zerobase.user.model.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SignUpCustomerServiceTest {
+
+    @Autowired
+    private SignUpCustomerService signUpCustomerService;
+
+    @Test
+    void SignUp() {
+        // given
+        SignUpForm form = SignUpForm.builder()
+                .name("name")
+                .birth(LocalDate.now())
+                .email("abc@gmail.com")
+                .password("1234")
+                .phone("01000000000")
+                .build();
+
+        // when
+        Customer customer = signUpCustomerService.signUp(form);
+
+        // then
+        assertNotNull(customer.getId());
+        assertNotNull(customer.getCreatedAt());
+    }
+}
