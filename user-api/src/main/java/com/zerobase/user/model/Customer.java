d35273a94e05a7cf9d0b3e0aa903317e17292ff0
MyungHwanKim <code.kmh9250@gmail.com>
2022-08-19 14:04:53 +0900

    feat : [003-Email-Verification] customer에 대한 email verification 구현

--- user-api/src/main/java/com/zerobase/user/model/Customer.java
+++ user-api/src/main/java/com/zerobase/user/model/Customer.java
@@ -10,7 +10,6 @@ import java.time.LocalDateTime;
 import java.util.Locale;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
--- user-api/src/main/java/com/zerobase/user/repository/CustomerRepository.java
+++ user-api/src/main/java/com/zerobase/user/repository/CustomerRepository.java
@@ -4,10 +4,6 @@ import com.zerobase.user.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-    Optional<Customer> findByEmail(String email);
 }
